67b76fa5b204dee19137514b6e60eff6a4861fa2
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-17 03:57:54 +0000

    added function for stdinput, implemented a user input function to start reading inputs from the client

--- play.js
+++ play.js
@@ -3,3 +3,25 @@ console.log('Connecting ...');
 
 connect();
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+ const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => handleUserInput(key));
+  return stdin;
+};
+
+setupInput();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+};
