3a7dbb7ece5bfc6b06bab8b79ec940be1eceb843
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-17 03:28:21 +0000

    moved client connect code to seperate module

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log(data);
+  })
+
+  return conn;
+}
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,25 +1,5 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-
-  console.log(conn);
-  return conn;
-}
-
+const { connect } = require('./client.js');
 console.log('Connecting ...');
+
 connect();
 
