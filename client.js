04690f7aef87421ead1be816250274151e8670ce
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-17 03:34:53 +0000

    added message for successful connection, and sent name to server for Snake

--- client.js
+++ client.js
@@ -12,6 +12,11 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  conn.on('connect', () => {
+    console.log("Connection to server established.");
+    conn.write('Name: JWW');
+  })
+
   conn.on('data', (data) => {
     console.log(data);
   })
